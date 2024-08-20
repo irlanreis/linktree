546421c962fbd1f7e5dfb8a21ef7457ba24437ce
irlanreis <xuiexpress@gmail.com>
2024-08-20 11:36:31 -0400

    Feat: Adiciona componente de foto de Perfil

---
+++ src/Icone-perfil.tsx
@@ -0,0 +1,11 @@
+import perfil from './assets/perfil-01.png';
+
+const IconePerfil = () => {
+  return (
+    <div>
+      <img className="rounded-full w-28 h-28 mr-auto ml-auto m-8" src={perfil} alt="Irlan Reis"  />
+    </div>
+  );
+}
+
+export default IconePerfil;
